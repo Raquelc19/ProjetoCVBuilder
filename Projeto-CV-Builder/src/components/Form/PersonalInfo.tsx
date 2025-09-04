d89604352ac8421b3bf4ca41dd56010addc238da
Layssa Oliveira <layssaoliveira626@gmail.com>
2025-09-04 07:14:02 -0300

    feita parte de melhoria de texto com integração da API

--- Projeto-CV-Builder/src/components/Form/PersonalInfo.tsx
+++ Projeto-CV-Builder/src/components/Form/PersonalInfo.tsx
@@ -1,36 +1,81 @@
+import { useState } from "react";
 import { MdSupervisorAccount } from "react-icons/md";
+import { upgradeResume } from "../../services/aiService"
 
 function PersonalInfo() {
-    return(
-        <div className="dados-pessoais m-5 text-gray-800">
-            <div className="titulo-dados-pessoais flex"><MdSupervisorAccount className="text-3xl"/><h2 className="font-semibold text-xl mt-0.4">Dados Pessoais</h2></div>
-              <p className="text-gray-600 text-sm mb-2">Informações básicas para contato</p>
-              <hr className="text-gray-300 mb-4"/>
-            <div className="area-nome flex flex-col">
-              <label className="info-dados">Nome Completo:*</label>
-              <input type="text " placeholder="Digite seu nome completo"></input>
+  const [resumoProfissional, setResumoProfissional] = useState("");
+  const [loading, setLoading] = useState(false);
+  const apiKey = localStorage.getItem("openai_api_key") || "";
+
+  const melhorarTextoProfissional = async (): Promise<void> => {
+    if (!resumoProfissional.trim()) {
+      alert("É necessário digitar um resumo profissional primeiro.")
+      return
+    }
+
+    if (!apiKey) {
+      alert("É necessário informar uma chave da API primeiro.")
+    }
+
+    setLoading(true);
+    try {
+      const textoMelhorado = await upgradeResume(resumoProfissional, apiKey);
+      setResumoProfissional(textoMelhorado);
+    } catch (error: any) {
+        console.error(error);
+        alert("Erro ao melhorar o texto.");
+    } finally {
+        setLoading(false);
+    }
+  };
+
+  return (
+      <div className="dados-pessoais m-5 text-gray-800">
+          <div className="titulo-dados-pessoais flex"><MdSupervisorAccount className="text-3xl"/><h2 className="font-semibold text-xl mt-0.4">Dados Pessoais</h2></div>
+            <p className="text-gray-600 text-sm mb-2">Informações básicas para contato</p>
+            <hr className="text-gray-300 mb-4"/>
+          <div className="area-nome flex flex-col">
+            <label className="info-dados">Nome Completo:*</label>
+            <input type="text " placeholder="Digite seu nome completo"></input>
+          </div>
+          <div className="area-email flex flex-col">
+            <label className="info-dados">E-mail:*</label>
+            <input type="email" placeholder="seu.email@exemplo.com"></input>
+          </div>
+          <div className="area-tel-lkdin flex">
+            <div className="flex flex-col mr-4 w-full">
+              <label className="info-dados">Telefone:*</label>
+              <input type="text" placeholder="(99)99999-9999"></input>
             </div>
-            <div className="area-email flex flex-col">
-              <label className="info-dados">E-mail:*</label>
-              <input type="email" placeholder="seu.email@exemplo.com"></input>
+            <div className="flex flex-col w-full">
+              <label className="info-dados">Linkedin:*</label>
+              <input type="email" placeholder="linkedin.com/in/seuperfil"></input>
             </div>
-            <div className="area-tel-lkdin flex">
-              <div className="flex flex-col mr-4 w-full">
-                <label className="info-dados">Telefone:*</label>
-                <input type="text" placeholder="(99)99999-9999"></input>
-              </div>
-              <div className="flex flex-col w-full">
-                <label className="info-dados">Linkedin:*</label>
-                <input type="email" placeholder="linkedin.com/in/seuperfil"></input>
-              </div>
-            </div>
-            <div className="resum-profissional flex flex-col">
+          </div>
+          <div className="resum-profissional flex flex-col">
+            <div className="flex">
               <label className="info-resumo">Resumo Profissional:</label>
-              <textarea placeholder="Descreva brevemente suas competências, experiências e objetivos..."></textarea>
-              <p>Este resumo aparecerá no topo do currículo</p>
+              <button 
+              className="ml-55"
+              onClick={melhorarTextoProfissional}
+              //utilizamos essa propriedade para deixar o botão de carregando desabilidade enquanto está carregando
+              disabled={loading}
+              >
+                {loading ? "Melhorando..." : "Melhorar texto"} 
+              </button>
             </div>
-        </div>   
-    );
+
+            <textarea 
+              value={resumoProfissional}
+              onChange={(e) => setResumoProfissional(e.target.value)}
+              placeholder="Descreva brevemente suas competências, experiências e objetivos..." 
+              className="mt-2 p-2 border rounded"
+            />
+
+            <p>Este resumo aparecerá no topo do currículo</p>
+          </div>
+      </div>   
+  );
 }
 
 export default PersonalInfo
