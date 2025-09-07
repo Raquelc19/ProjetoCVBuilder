03d19c8956aa7e0c0615c2d0d35b313d16650ddc
Layssa Oliveira <layssaoliveira626@gmail.com>
2025-09-06 22:58:17 -0300

    unido projeto geral com os ajustes de front feitos pela naiade

--- Projeto-CV-Builder/src/pages/curriculo.tsx
+++ Projeto-CV-Builder/src/pages/curriculo.tsx
@@ -18,22 +18,26 @@ function CurriculumPreview() {
   const [resumoProfissional, setResumoProfissional] = useState<string>("");
 
   return (
-    <main className="min-h-screen w-screen grid place-items-center bg-[url('background.png')] bg-cover bg-center bg-[#fcfbfb] ">
+    <>
+      <main className="min-h-screen w-screen grid place-items-center bg-[url('background.png')] bg-cover bg-center bg-[#fcfbfb] ">
         <HeaderSection exportPdf={exportarPdf}/>
-      <div className="flex justify-center w-[100vw] max-w-[1200px] h-[80vh] gap-6 mx-auto">
-        {/* --- Formulário --- */}
-        <FormSection experiences={experiences} setExperiences={setExperiences} skills={skills} setSkills ={setSkills} 
-            Nome= {Nome} setNome= {setNome}
-            email= {email} setEmail={setEmail}
-            telefone= {telefone} setTelefone={setTelefone}
-            linkedin= {linkedin} setLinkedin={setLinkedin}
-            resumoProfissional= {resumoProfissional} setResumoProfissional={setResumoProfissional}>
-          <Skills skills={skills} setSkills ={setSkills} />
-        /</FormSection>
-        {/* --- Preview --- */}
-        <PreviewSection  experiences={experiences} skills={skills} Nome={Nome} email={email} telefone= {telefone} linkedin= {linkedin} resumoProfissional={resumoProfissional} ref={contentDocument}/>
-      </div>
-    </main>
+        <div className="flex justify-center w-[100vw] max-w-[1200px] h-[80vh] gap-6 mx-auto">
+          {/* --- Formulário --- */}
+          <FormSection experiences={experiences} setExperiences={setExperiences} skills={skills} setSkills ={setSkills} 
+              Nome= {Nome} setNome= {setNome}
+              email= {email} setEmail={setEmail}
+              telefone= {telefone} setTelefone={setTelefone}
+              linkedin= {linkedin} setLinkedin={setLinkedin}
+              resumoProfissional= {resumoProfissional} setResumoProfissional={setResumoProfissional}>
+            <Skills skills={skills} setSkills ={setSkills} />
+          /</FormSection>
+          {/* --- Preview --- */}
+          <PreviewSection  experiences={experiences} skills={skills} Nome={Nome} email={email} telefone= {telefone} linkedin= {linkedin} resumoProfissional={resumoProfissional} ref={contentDocument}/>
+        </div>
+        <span className="text-gray-800">&copy; 2025 Devriders. Todos os direitos reservados. </span>
+      </main>
+    </>
+    
   );
 }
 
